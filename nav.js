94b9dfedc69315f932f9b7441415201bbecb1b9b
Karen Zhou <karenzhou@uchicago.edu>
2026-03-06 17:55:45 -0600

    refactor nav bar, update video and imgs

---
+++ nav.js
@@ -0,0 +1,14 @@
+document.getElementById('nav').innerHTML = `
+<nav class="nav">
+  <div class="container">
+    <a href="/" class="nav-brand">
+      <img src="assets/ac_dark.png" alt="" class="nav-brand-icon">
+      <span class="nav-brand-text">AutoChecklist</span>
+    </a>
+    <ul class="nav-links">
+      <li><a href="docs/">Docs</a></li>
+      <li><a href="/">Paper</a></li>
+      <li><a href="https://github.com/ChicagoHAI/AutoChecklist">GitHub</a></li>
+    </ul>
+  </div>
+</nav>`;
